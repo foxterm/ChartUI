ad9552fab581ad68b714c1ee4ccc6d068d1e4103
FoxTerm <foxterm@foxmail.com>
2026-09-07 00:54:31 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "ChartUI",
     platforms: [
         .iOS(.v16),
-        .macOS(.v13),
+        .macOS(.v14),
     ],
     products: [
         .library(name: "ChartUI", targets: ["ChartUI"]),
